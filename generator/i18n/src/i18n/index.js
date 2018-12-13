f3081ffe916145ea352f6d997103ee1c086e5917
caowenpeng <caowenpeng66@163.com>
2018-12-13 22:03:51 +0800

    修改MD文档&处理i18n的messages的数据结构

--- generator/i18n/src/i18n/index.js
+++ generator/i18n/src/i18n/index.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 
-const context = require.context('./languages', true, /\.js$/)//webpack的context 
+const context = require.context('./languages', true, /\.js$/)
 const messages = {}
 context.keys().forEach((item) => {
   const url = item.split('/')
   const lang = url[1]
   const module = url[2].slice(0, -3)
-  let concatObj = messages[lang] ? messages[lang] : {}
-  let langSource = require(`./languages/${lang}/${module}`).default
-  messages[lang] = Object.assign(concatObj, langSource)//messages对象合并
+  if (!messages[lang]) messages[lang] = {}
+  messages[lang][module] = require(`./languages/${lang}/${module}`).default
 })
 export default new VueI18n({
   locale: 'cn',
