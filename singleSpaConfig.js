f7beaf887ff3c178509f5e7f57f9bed5bbf9ba61
qinyang <wannengxin@gmail.com>
2019-11-26 15:42:47 +0800

    Patch qy (#16)
    * fix: 暂时不提取公共包
    
    * 2.0.6

--- singleSpaConfig.js
+++ singleSpaConfig.js
@@ -18,28 +18,29 @@ module.exports = function singleSpaConfig(api) {
       splitChunks: false,
     }
 
-    const ExternalsList = [
-      'vue',
-      'vuex',
-      'vue-router',
-      // 'vue-rx',
-      'axios',
-      // '@rishiqing/sdk',
-      '@rishiqing/kite-design/dist/kite-basic',
-      '@rishiqing/kite-design/dist/kite-business',
-      'kite-basic',
-      'kite-business',
-      '@rishiqing/kite-design/dist/kite-basic.css',
-      '@rishiqing/kite-design/dist/kite-business.css',
-    ]
+    // const ExternalsList = [
+    //   'vue',
+    //   'vuex',
+    //   'vue-router',
+    //   // 'vue-rx',
+    //   'axios',
+    //   // '@rishiqing/sdk',
+    //   '@rishiqing/kite-design/dist/kite-basic',
+    //   '@rishiqing/kite-design/dist/kite-business',
+    //   'kite-basic',
+    //   'kite-business',
+    //   '@rishiqing/kite-design/dist/kite-basic.css',
+    //   '@rishiqing/kite-design/dist/kite-business.css',
+    // ]
 
     webpackConfig.externals = [
-      function externals(context, request, callback) {
-        if (ExternalsList.includes(request)) {
-          return callback(null, `var window.app.require('${request}')`)
-        }
-        callback()
-      },
+      // 暂时屏蔽掉提取公共包，因为会导致版本依赖的问题
+      // function externals(context, request, callback) {
+      //   if (ExternalsList.includes(request)) {
+      //     return callback(null, `var window.app.require('${request}')`)
+      //   }
+      //   callback()
+      // },
     ]
 
     webpackConfig.plugins.push(new WebpackSystemRegister({
