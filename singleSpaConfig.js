b5042caef1c71ac5268fdde74f2b192102602c63
qinyang <wannengxin@gmail.com>
2020-05-27 14:53:56 +0800

    Patch qy 0527 (#45)
    * fix: 优化singleSpa构建方案
    
    * 3.0.0-next.9

--- singleSpaConfig.js
+++ singleSpaConfig.js
@@ -1,7 +1,9 @@
 const WebpackSystemRegister = require('@rishiqing/webpack-system-register')
-const ExternalsList = require('@rishiqing/vue-package/lib/externalList')
+const VuePackageExtarnal = require('@rishiqing/vue-package-external')
 
 module.exports = function singleSpaConfig(api) {
+  const isDev = process.env.NODE_ENV === 'development'
+
   api.chainWebpack((webpackChain) => {
     webpackChain.plugins.delete('html')
     webpackChain.plugins.delete('preload')
@@ -9,9 +11,12 @@ module.exports = function singleSpaConfig(api) {
   })
 
   api.configureWebpack((webpackConfig) => {
-    webpackConfig.entry = {
-      app: './src/main.js',
-    }
+    // 不再对 entry.app 重新赋值了
+    // 如果entry.app是个数组，现在 @rishiqing/webpack-system-register 也能正常处理
+    // 之前entry.app如果是数组，会出问题
+    // webpackConfig.entry = {
+    //   app: './src/main.js',
+    // }
     webpackConfig.output.filename = '[name].js'
 
     webpackConfig.optimization = {
@@ -19,20 +24,20 @@ module.exports = function singleSpaConfig(api) {
       splitChunks: false,
     }
 
-    webpackConfig.externals = [
-      function externals(context, request, callback) {
-        if (ExternalsList.includes(request)) {
-          return callback(null, `var window.__rsq_common_package__.require('${request}')`)
-        }
-        callback()
-      },
-    ]
+    // 只有构建线上环境的时候才抽取公共依赖包，方便本地调试
+    if (!isDev) {
+      webpackConfig.externals = [
+        VuePackageExtarnal(),
+      ]
+    }
 
     // 打包的时候，忽略掉一些包
     webpackConfig.module.rules.push({
       test: [
-        // 忽略kite-design里的css
-        /@rishiqing\/kite-design\/.*\.css$/,
+        ...isDev ? [] : [
+          // 忽略kite-design里的css (只在线上环境才去除，开发环境不去除)
+          /@rishiqing\/kite-design\/.*\.css$/,
+        ],
         // 忽略systemjs，打包成微应用，不能再引入systemjs
         /systemjs/,
       ],
